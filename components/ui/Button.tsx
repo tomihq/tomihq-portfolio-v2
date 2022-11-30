13e3ad61fe8ab6d269b20375c24040d955ae8dcd
= <hernandeztomas584@gmail.com>
2022-11-30 03:06:14 -0300

    add: Download CV button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -2,13 +2,15 @@ import {ReactElement} from 'react';
 interface ButtonProps {
     children: React.ReactNode
     buttonClasses: string
+    onClick?: any;
 }
 
-export const Button = ({children, buttonClasses}:ButtonProps):ReactElement => {
+export const Button = ({children, buttonClasses, onClick = null}:ButtonProps):ReactElement => {
     return(
         <button
                   type="button"
                   className={buttonClasses}
+                  onClick={onClick}
                 >
                    <span>
                         {children}
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import { Button } from '../components/ui/Button';
 import { AvatarPicture } from '../components/ui/AvatarPicture';
 const Home: NextPage = () => {
 
+
     const hue = keyframes`
   from {
     -webkit-filter: hue-rotate(0deg);
@@ -68,6 +69,7 @@ const Home: NextPage = () => {
                     </Link>
                 </Button>
                 
+                
               </div>
             </section>
 
@@ -104,6 +106,17 @@ const Home: NextPage = () => {
 
                 </div>
             </div>
+            <Button
+                  buttonClasses='border border-gray-700 bg-black text-white  rounded-md mt-4 px-4 py-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline dark:bg-black dark:border-neutral-400 dark:hover:bg-gray-800
+                  text-black dark:text-white'
+                  
+                >
+            
+                 <Link href="files/cv.pdf" passHref>
+                      <a target="_blank" download="Tomás Hernández CV">      Download CV   </a>
+                    </Link>
+            </Button>
+
         </section>
 
       </Layout>
