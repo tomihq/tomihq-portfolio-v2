e67dd5170d8283a53e8381277b9c4b99cce940f2
= <hernandeztomas584@gmail.com>
2022-11-30 03:07:34 -0300

    modified: button color

--- pages/index.tsx
+++ pages/index.tsx
@@ -107,8 +107,8 @@ const Home: NextPage = () => {
                 </div>
             </div>
             <Button
-                  buttonClasses='border border-gray-700 bg-black text-white  rounded-md mt-4 px-4 py-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline dark:bg-black dark:border-neutral-400 dark:hover:bg-gray-800
-                  text-black dark:text-white'
+                  buttonClasses='border border-gray-700 bg-black text-white  rounded-md mt-4 px-4 py-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline dark:bg-white dark:border-neutral-400 dark:hover:bg-gray-800
+                  text-black dark:text-black'
                   
                 >
             
